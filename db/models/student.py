9fe7081d0f41598e53934bdc254a2ffae405b5c2
NoelJoost <noel.joost@ksb-sg.ch>
2021-01-12 14:58:37 +0100

    MATCHD-17: add student model

---
+++ db/models/student.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class Student(models.Model):
+    user = models.OneToOneField(to=get_user_model(), on_delete=models.CASCADE, related_name='student')
+    mobile_number = models.CharField(max_length=10, blank=False)
