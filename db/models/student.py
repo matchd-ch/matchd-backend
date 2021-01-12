4fa3fd2954826a1a342b0428bc3f2edac743addc
NoelJoost <noel.joost@ksb-sg.ch>
2021-01-12 15:23:30 +0100

    MATCHD-17: adapt student model

--- db/models/student.py
+++ db/models/student.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class Student(models.Model):
     user = models.OneToOneField(to=get_user_model(), on_delete=models.CASCADE, related_name='student')
-    mobile_number = models.CharField(max_length=10, blank=False)
+    mobile_number = models.CharField(max_length=12, blank=False)
