d20f77c7ef50bfbf6673215a42cfbec86c3b40e5
NoelJoost <noel.joost@ksb-sg.ch>
2021-01-12 16:15:59 +0100

    MATCHD-17: add scheme for student registration

--- api/schema/registration/schema.py
+++ api/schema/registration/schema.py
@@ -6,7 +6,7 @@ from graphql_auth.mutations import Register
 from django.utils.translation import gettext_lazy as _
 
 from api.schema.error import ErrorType
-from db.models import Company
+from db.models import Company, Student
 
 
 class CompanyInput(graphene.InputObjectType):
@@ -47,6 +47,42 @@ class RegisterCompany(Register):
         return result
 
 
+class StudentInput(graphene.InputObjectType):
+    mobile_number = graphene.String(description=_('MobileNumber'))
+
+
+# pylint: disable=R0903
+# pylint: disable=R0901
+class RegisterStudent(Register):
+
+    class Arguments:
+        student = StudentInput(description=_('Student is required.'), required=True)
+
+    class Meta:
+        description = _('Creates a new user as student')
+
+    @classmethod
+    def mutate(cls, root, info, **data):
+        try:
+            student = Student(**data.get('student'))
+            student.full_clean(exclude=['user'])
+        except ValidationError as error:
+            return RegisterStudent(success=False, errors=ErrorType.serialize(error.message_dict))
+
+        result = cls.resolve_mutation(root, info, **data)
+        if result.errors:
+            return RegisterStudent(success=False, errors=ErrorType.serialize(result.errors))
+
+        try:
+            user = get_user_model().objects.get(email=data.get('email'))
+        except get_user_model().DoesNotExist:
+            return RegisterCompany(success=False, errors={'non_field_errors': 'User not found.'})
+        student.user = user
+        student.save()
+        return result
+
+
 class RegistrationMutation(graphene.ObjectType):
     register_company = RegisterCompany.Field()
+    register_student = RegisterStudent.Field()
     verify_account = mutations.VerifyAccount.Field()
