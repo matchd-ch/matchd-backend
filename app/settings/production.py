a3bde07705ea3b93e944b5ce930d6754ba43f112
Stefan Suter <stefan.suter@liip.ch>
2021-01-14 23:55:40 +0100

    MATCHD-46: add deployment development

--- app/settings/production.py
+++ app/settings/production.py
@@ -12,9 +12,7 @@ DATABASES = {
         },
         'NAME': os.getenv('DJANGO_DB_NAME', ''),
         'USER': os.getenv('DJANGO_DB_USER', ''),
-        'PASSWORD': os.getenv('DJANGO_DB_PW', ''),
-        'HOST': os.getenv('DJANGO_DB_HOST', ''),
-        'PORT': os.getenv('DJANGO_DB_PORT', ''),
+        'PASSWORD': os.getenv('DJANGO_DB_PW', '')
     }
 }
 
