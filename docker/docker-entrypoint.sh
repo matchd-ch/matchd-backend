677484da74211476cf92fcbc80df30108588dccb
Stefan Suter <stefan.suter@liip.ch>
2021-01-15 00:09:31 +0100

    MATCHD-46: add deployment development

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "HOST: ${DJANGO_DB_HOST}";
-echo "DATABASE: ${DJANGO_DB_PORT}";
+echo "Apply database migrations"
+python ./manage.py migrate
 
-# Check if script was called by CMD, can be sh -c 'CMD' or CMD
-if [ "$1" = '/usr/libexec/s2i/run' ] || [ "$3" = '/usr/libexec/s2i/run' ] || [ "$2" = 'runserver' ]; then
-    # Wait for the database to be available
-    # until nc -vzw 2 $DJANGO_DB_HOST $DJANGO_DB_PORT; do echo "mysql is not available. waiting..." && sleep 2; done
+echo "Collect static files"
+python ./manage.py collectstatic --noinput
 
-    echo "Apply database migrations"
-    python ./manage.py migrate
+echo "Setup super admin"
+./manage.py loaddata app/fixtures/users.json
 
-    echo "Collect static files"
-    python ./manage.py collectstatic --noinput
-
-    echo "Setup super admin"
-    ./manage.py loaddata app/fixtures/users.json
-
-    # Wait for elastic search to be available
-    until curl -s $DJANGO_ELASTIC_SEARCH_URL/_cluster/health | egrep '(green|yellow)' -i > /dev/null; do echo "elastic is not ready. waiting..." && sleep 5; done
+# Wait for elastic search to be available
+until curl -s $DJANGO_ELASTIC_SEARCH_URL/_cluster/health | egrep '(green|yellow)' -i > /dev/null; do echo "elastic is not ready. waiting..." && sleep 5; done
 
 #    TODO add initial data
 #    echo "Load Demo data"
@@ -27,6 +19,6 @@ if [ "$1" = '/usr/libexec/s2i/run' ] || [ "$3" = '/usr/libexec/s2i/run' ] || [ "
 #
 #    echo "Reindex elastic"
 #    python ./manage.py update_index
-fi
+
 
 exec "$@"
