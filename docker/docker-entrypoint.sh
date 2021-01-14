d0df69a472ad972ec2741cdb7459a40c142b65c0
Stefan Suter <stefan.suter@liip.ch>
2021-01-15 00:01:03 +0100

    MATCHD-46: add deployment development

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,7 +7,7 @@ echo "DATABASE: ${DJANGO_DB_PORT}";
 # Check if script was called by CMD, can be sh -c 'CMD' or CMD
 if [ "$1" = '/usr/libexec/s2i/run' ] || [ "$3" = '/usr/libexec/s2i/run' ] || [ "$2" = 'runserver' ]; then
     # Wait for the database to be available
-    until nc -vzw 2 $DJANGO_DB_HOST $DJANGO_DB_PORT; do echo "mysql is not available. waiting..." && sleep 2; done
+    # until nc -vzw 2 $DJANGO_DB_HOST $DJANGO_DB_PORT; do echo "mysql is not available. waiting..." && sleep 2; done
 
     echo "Apply database migrations"
     python ./manage.py migrate
