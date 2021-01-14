1dcd70a18c58ff3c600c8c88b41656370122ee51
Stefan Suter <stefan.suter@liip.ch>
2021-01-15 00:10:26 +0100

    MATCHD-46: add deployment development

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,15 +10,11 @@ python ./manage.py collectstatic --noinput
 echo "Setup super admin"
 ./manage.py loaddata app/fixtures/users.json
 
-# Wait for elastic search to be available
-until curl -s $DJANGO_ELASTIC_SEARCH_URL/_cluster/health | egrep '(green|yellow)' -i > /dev/null; do echo "elastic is not ready. waiting..." && sleep 5; done
-
 #    TODO add initial data
 #    echo "Load Demo data"
 #    ./manage.py loaddata db/fixtures/initial_data.json
 #
 #    echo "Reindex elastic"
 #    python ./manage.py update_index
-
 
 exec "$@"
