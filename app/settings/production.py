35085c54e4a137840772d6a4680edd637821c83a
Stefan Suter <stefan.suter@liip.ch>
2021-01-18 23:03:52 +0100

    MATCHD-46: adapt database connection for production, disable smtp

--- app/settings/production.py
+++ app/settings/production.py
@@ -3,6 +3,21 @@ from .base import *
 DEBUG = False
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'unix_socket': '/var/run/mysqld/mysqld.sock',
+        },
+        'NAME': os.getenv('DJANGO_DB_NAME', ''),
+        'USER': os.getenv('DJANGO_DB_USER', ''),
+        'PASSWORD': os.getenv('DJANGO_DB_PW', '')
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 try:
     from .local import *
 except ImportError:
