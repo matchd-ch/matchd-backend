3a03de8a14990e71115ddb82b79a353d358ea11f
Stefan Suter <stefan.suter@liip.ch>
2021-01-18 12:54:42 +0100

    MATCHD-46: deployment Dockerfile

--- docker/deployment/docker-entrypoint.sh
+++ docker/deployment/docker-entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-# Check if script was called by CMD, can be sh -c 'CMD' or CMD
-if [ "$1" = '/usr/libexec/s2i/run' ] || [ "$3" = '/usr/libexec/s2i/run' ] || [ "$2" = 'runserver' ]; then
-    echo "Apply database migrations"
-    python ./manage.py migrate
+echo "Apply database migrations"
+python ./manage.py migrate
 
-    echo "Collect static files"
-    python ./manage.py collectstatic --noinput
+echo "Collect static files"
+python ./manage.py collectstatic --noinput
 
-    echo "Reindex elastic"
-    python ./manage.py update_index
-fi
+echo "Reindex elastic"
+python ./manage.py update_index
 
 exec "$@"
