f016f8a1568f7c994d70d009056d4db8a195fc9e
NoelJoost <noel.joost@ksb-sg.ch>
2021-01-21 12:22:29 +0100

    MATCHD-79: AuthMutation clean Up

--- api/schema/logIn/schema.py
+++ api/schema/logIn/schema.py
@@ -5,17 +5,6 @@ from graphql_auth import mutations
 
 
 class AuthMutation(graphene.ObjectType):
-    register = mutations.Register.Field()
-    verify_account = mutations.VerifyAccount.Field()
     token_auth = mutations.ObtainJSONWebToken.Field()
 
 
-class Query(UserQuery, MeQuery, graphene.ObjectType):
-    pass
-
-
-class Mutation(AuthMutation, graphene.ObjectType):
-    pass
-
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
