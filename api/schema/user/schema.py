474feb8cb07fc06e3f0d3d5501bac47e51b5cc5b
Stefan Suter <stefan.suter@liip.ch>
2021-01-25 17:57:25 +0100

    MATCHD-17: add me endpoint

---
+++ api/schema/user/schema.py
@@ -0,0 +1,24 @@
+import graphene
+from django.contrib.auth import get_user_model
+from graphql_auth.schema import UserNode
+from graphql_auth.settings import graphql_auth_settings
+
+
+class UserWithProfileNode(UserNode):
+
+    class Meta:
+        model = get_user_model()
+        filter_fields = graphql_auth_settings.USER_NODE_FILTER_FIELDS
+        exclude = graphql_auth_settings.USER_NODE_EXCLUDE_FIELDS
+        interfaces = (graphene.relay.Node,)
+        skip_registry = True
+
+
+class UserQuery(graphene.ObjectType):
+    me = graphene.Field(UserWithProfileNode)
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_authenticated:
+            return user
+        return None
