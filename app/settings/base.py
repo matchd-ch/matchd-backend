04f95b29ea8751b3153449dc721044843233f87d
NoelJoost <noel.joost@ksb-sg.ch>
2021-01-25 15:39:48 +0100

    MATCHD-79: remove django_filters

--- app/settings/base.py
+++ app/settings/base.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters',
     'db.apps.DbConfig',
     'api.apps.ApiConfig'
 ]
