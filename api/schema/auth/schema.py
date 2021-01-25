6185c504956f43b1fafbab2fcaaf0e24741d3f60
NoelJoost <noel.joost@ksb-sg.ch>
2021-01-25 11:56:02 +0100

    MATCHD-79: add RefreshToken

--- api/schema/auth/schema.py
+++ api/schema/auth/schema.py
@@ -1,9 +1,10 @@
 import graphene
+import graphql_jwt
 
-from graphql_auth.schema import UserQuery, MeQuery
 from graphql_auth import mutations
 
 
 class AuthMutation(graphene.ObjectType):
     token_auth = mutations.ObtainJSONWebToken.Field()
     refresh_token = mutations.RefreshToken.Field()
+    revoke_token = graphql_jwt.Revoke.Field()
