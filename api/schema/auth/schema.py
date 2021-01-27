92a50eec135d16ae8038252c945a3e4fe55b45c2
NoelJoost <noel.joost@ksb-sg.ch>
2021-01-27 18:20:57 +0100

    MATCHD-30: add Password Reset

--- api/schema/auth/schema.py
+++ api/schema/auth/schema.py
@@ -10,3 +10,5 @@ class AuthMutation(graphene.ObjectType):
     token_auth = mutations.ObtainJSONWebToken.Field()
     refresh_token = mutations.RefreshToken.Field()
     revoke_token = graphql_jwt.Revoke.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
+    password_reset = mutations.PasswordReset.Field()
