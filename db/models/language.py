02a15342df279e4e9074638bc838b64f6ce08c26
NoelJoost <noel.joost@ksb-sg.ch>
2021-02-03 14:05:21 +0100

    MATCHD-75: alter name in skill, language, languageNiveau

--- db/models/language.py
+++ db/models/language.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Language(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
--- db/models/skill.py
+++ db/models/skill.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Skill(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,unique=True)
