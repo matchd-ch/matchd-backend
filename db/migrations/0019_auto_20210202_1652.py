ca1dd8e4f887298809db7ec44606c776805a018e
Stefan Suter <stefan.suter@liip.ch>
2021-02-03 17:12:12 +0100

    MATCHD-44: adapt migration order

--- db/migrations/0019_auto_20210202_1652.py
+++ db/migrations/0019_auto_20210202_1652.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('db', '0007_auto_20210201_1331'),
+        ('db', '0018_auto_20210203_1534'),
     ]
 
     operations = [
