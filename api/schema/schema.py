59aae63db8657935d699db6ecc056d672e04ce7d
NoelJoost <noel.joost@ksb-sg.ch>
2021-02-03 11:32:05 +0100

    MATCHD-75: adjust LanguageQuery & LanguageNiveauQuery

--- api/schema/schema.py
+++ api/schema/schema.py
@@ -1,6 +1,8 @@
 import graphene
 
+from api.schema.language import LanguageQuery
 from api.schema.auth import AuthMutation, LogoutMutation, VerifyPasswordResetToken
+from api.schema.language_niveau import LanguageNiveauQuery
 from api.schema.skill import SkillQuery
 from api.schema.registration import RegistrationMutation
 from api.schema.user import UserQuery
@@ -16,7 +18,7 @@ class Mutation(
     pass
 
 
-class Query(VerifyPasswordResetToken, UserQuery, SkillQuery):
+class Query(VerifyPasswordResetToken, UserQuery, SkillQuery, LanguageQuery, LanguageNiveauQuery):
     pass
 
 
