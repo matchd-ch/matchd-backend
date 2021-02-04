8106c78c162556bdedea6093a1f3ec91eb90830e
NoelJoost <noel.joost@ksb-sg.ch>
2021-02-04 19:10:41 +0100

    MATCHD-75: add hobby schema

---
+++ api/schema/hobby/schema.py
@@ -0,0 +1,26 @@
+import graphene
+from graphene import ObjectType
+from graphene_django import DjangoObjectType
+
+from db.models import Hobby
+
+
+class HobbyType(DjangoObjectType):
+    class Meta:
+        model = Hobby
+
+
+class HobbyQuery(ObjectType):
+    hobby = graphene.List(HobbyType)
+
+    def resolve_hobby(self, info, **kwargs):
+        return Hobby.objects.all()
+
+
+class HobbyInputType(graphene.InputObjectType):
+    id = graphene.Int()
+    name = graphene.String(required=True)
+
+    @property
+    def pk(self):
+        return self.id
