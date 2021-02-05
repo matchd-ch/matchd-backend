389f6da9aba0670780ebd20d9784a9faeadea558
Stefan Suter <stefan.suter@liip.ch>
2021-02-05 07:51:28 +0100

    MATCHD-72: add tests, adapt settings

--- api/tests/user/student.py
+++ api/tests/user/student.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from django.contrib.auth import get_user_model
 from graphene_django.utils import GraphQLTestCase
@@ -8,35 +9,144 @@ from api.schema import schema
 from db.models import Student
 
 
+# pylint:disable=R0913
 class StudentGraphQLTestCase(GraphQLTestCase):
     GRAPHQL_SCHEMA = schema
 
+    query_step_1 = '''
+            mutation StudentProfileMutation($step1: StudentProfileInputStep1) {
+              studentProfileStep1(step1: $step1) {
+                success,
+                errors
+              }
+            }
+            '''
+
+    variables_step_1 = {
+              'step1': {
+                'firstName': 'John2',
+                'lastName': 'Doe2',
+                'street': 'Doestreet 55',
+                'zip': '9000',
+                'city': 'St. Gallen',
+                'dateOfBirth': '01.01.2000',
+                'mobile': '+41999999999'
+              }
+            }
+
+    invalid_variables_step_1 = {
+        'step1': {
+            'firstName': '',
+            'lastName': '',
+            'street': '',
+            'zip': '',
+            'city': '',
+            'dateOfBirth': '',
+            'mobile': ''
+        }
+    }
+
+    query_step_5 = '''
+                mutation StudentProfileMutation($step5: StudentProfileInputStep5) {
+                  studentProfileStep5(step5: $step5) {
+                    success,
+                    errors,
+                    nicknameSuggestions
+                  }
+                }
+                '''
+
+    variables_step_5 = {
+        'step5': {
+            'nickname': 'jane_doe'
+        }
+    }
+
+    invalid_variables_step_5 = {
+        'step5': {
+            'nickname': ''
+        }
+    }
+
+    invalid_variables_step_5_nickname = {
+        'step5': {
+            'nickname': 'john_doe'
+        }
+    }
+
+    query_step_6 = '''
+            mutation StudentProfileMutation($step6: StudentProfileInputStep6) {
+              studentProfileStep6(step6: $step6) {
+                success,
+                errors
+              }
+            }
+            '''
+
+    variables_step_6 = {
+                'step6': {
+                    'state': 'anonymous'
+                }
+            }
+
+    invalid_variables_step_6 = {
+                'step6': {
+                    'state': 'anonymous2'
+                }
+            }
+
     def setUp(self):
-        self.user = get_user_model().objects.create(
+        self.student = get_user_model().objects.create(
             username='john@doe.com',
             email='john@doe.com',
             type='student'
         )
-        self.user.set_password('asdf1234$')
-        self.user.save()
+        self.student.set_password('asdf1234$')
+        self.student.save()
 
-        Student.objects.create(user=self.user, mobile='+41771234568')
+        Student.objects.create(user=self.student, mobile='+41771234568')
 
-        user_status = UserStatus.objects.get(user=self.user)
+        user_status = UserStatus.objects.get(user=self.student)
         user_status.verified = True
         user_status.save()
 
-    def _login(self):
+        self.company = get_user_model().objects.create(
+            username='john2@doe.com',
+            email='john2@doe.com',
+            type='company'
+        )
+        self.company.set_password('asdf1234$')
+        self.company.save()
+
+        user_status = UserStatus.objects.get(user=self.company)
+        user_status.verified = True
+        user_status.save()
+
+        self.student_with_nickname = get_user_model().objects.create(
+            username='john3@doe.com',
+            email='john3@doe.com',
+            type='student'
+        )
+        self.student_with_nickname.set_password('asdf1234$')
+        self.student_with_nickname.save()
+
+        Student.objects.create(user=self.student_with_nickname, mobile='+41771234568', nickname='john_doe')
+
+        user_status = UserStatus.objects.get(user=self.student_with_nickname)
+        user_status.verified = True
+        user_status.save()
+
+    def _login(self, username):
         response = self.query(
             '''
             mutation TokenAuth {
-                tokenAuth(username: "john@doe.com", password: "asdf1234$") {
+                tokenAuth(username: "%s", password: "asdf1234$") {
                     success,
                     errors,
                     token
                 }
             }
-            '''
+            ''' % username
         )
         self.assertResponseNoErrors(response)
         content = json.loads(response.content)
@@ -58,6 +168,10 @@ class StudentGraphQLTestCase(GraphQLTestCase):
                 type
                 student {
                     mobile
+                    zip
+                    street
+                    city
+                    dateOfBirth
                 }
               }
             }
@@ -77,107 +191,135 @@ class StudentGraphQLTestCase(GraphQLTestCase):
             self.assertResponseHasErrors(response)
             self.assertIsNone(content['data'].get('me'))
 
+    def _test_step_without_login(self, query, variables, error_key):
+        response = self.query(query, variables=variables)
+        content = json.loads(response.content)
+        self.assertResponseHasErrors(response)
+        self.assertIsNone(content['data'].get(error_key))
+
+    def _test_step_as_company(self, query, variables, error_key):
+        self._login('john2@doe.com')
+        response = self.query(query, variables=variables)
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        self.assertFalse(content['data'].get(error_key).get('success'))
+        self.assertIn('type', content['data'].get(error_key).get('errors'))
+
+    def _test_step_with_invalid_step(self, invalid_step, query, variables, error_key):
+        self._login('john@doe.com')
+        self.student.profile_step = invalid_step
+        self.student.save()
+        response = self.query(query, variables=variables)
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        self.assertIn('profileStep', content['data'].get(error_key).get('errors'))
+
+    def _test_step_with_invalid_data(self, step, query, variables, error_key, expected_errors):
+        self._login('john@doe.com')
+        self.student.profile_step = step
+        self.student.save()
+        response = self.query(query, variables=variables)
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        errors = content['data'].get(error_key).get('errors')
+        for expected_error in expected_errors:
+            self.assertIn(expected_error, errors)
+
+    def _test_and_get_step_response_content(self, step, query, variables, error_key, success=True):
+        self._login('john@doe.com')
+        self.student.profile_step = step
+        self.student.save()
+        response = self.query(query, variables=variables)
+        self.assertResponseNoErrors(response)
+        content = json.loads(response.content)
+        if success:
+            self.assertTrue(content['data'].get(error_key).get('success'))
+            self.assertIsNone(content['data'].get(error_key).get('errors'))
+        else:
+            self.assertFalse(content['data'].get(error_key).get('success'))
+            self.assertIsNotNone(content['data'].get(error_key).get('errors'))
+        return content
+
     def test_me(self):
-        self._login()
+        self._login('john@doe.com')
         self._test_me()
 
     def test_me_without_login(self):
         self._test_me(False)
 
+    def test_profile_step_1_without_login(self):
+        self._test_step_without_login(self.query_step_1, self.variables_step_1, 'studentProfileStep1')
+
+    def test_profile_step_5_without_login(self):
+        self._test_step_without_login(self.query_step_5, self.variables_step_5, 'studentProfileStep5')
+
     def test_profile_step_6_without_login(self):
-        response = self.query(
-            '''
-            mutation StudentProfileMutation($step6: StudentProfileStep6Input) {
-              studentProfileStep6(step6: $step6) {
-                success,
-                errors
-              }
-            }
-            ''', variables={
-                'step6': {
-                    'state': 'anonymous2'
-                }
-            }
+        self._test_step_without_login(self.query_step_6, self.variables_step_6, 'studentProfileStep6')
 
-        )
+    def test_profile_step_1_as_company(self):
+        self._test_step_as_company(self.query_step_1, self.variables_step_1, 'studentProfileStep1')
 
-        content = json.loads(response.content)
-        self.assertResponseHasErrors(response)
-        self.assertIsNone(content['data'].get('studentProfileStep6'))
+    def test_profile_step_5_as_company(self):
+        self._test_step_as_company(self.query_step_5, self.variables_step_5, 'studentProfileStep5')
+
+    def test_profile_step_6_as_company(self):
+        self._test_step_as_company(self.query_step_6, self.variables_step_6, 'studentProfileStep6')
+
+    def test_profile_step_1_with_invalid_step(self):
+        self._test_step_with_invalid_step(0, self.query_step_1, self.variables_step_1, 'studentProfileStep1')
+
+    def test_profile_step_5_with_invalid_step(self):
+        self._test_step_with_invalid_step(4, self.query_step_5, self.variables_step_5, 'studentProfileStep5')
 
     def test_profile_step_6_with_invalid_step(self):
-        self._login()
-        response = self.query(
-            '''
-            mutation StudentProfileMutation($step6: StudentProfileStep6Input) {
-              studentProfileStep6(step6: $step6) {
-                success,
-                errors
-              }
-            }
-            ''', variables={
-                'step6': {
-                    'state': 'anonymous2'
-                }
-            }
+        self._test_step_with_invalid_step(5, self.query_step_6, self.variables_step_6, 'studentProfileStep6')
 
-        )
+    def test_profile_step_1_with_invalid_data(self):
+        self._test_step_with_invalid_data(1, self.query_step_1, self.invalid_variables_step_1, 'studentProfileStep1',
+                                          ['firstName', 'lastName', 'city', 'zip', 'street', 'dateOfBirth', 'mobile'])
 
-        content = json.loads(response.content)
-        self.assertResponseNoErrors(response)
-        self.assertIn('profileStep', content['data'].get('studentProfileStep6').get('errors'))
+    def test_profile_step_5_with_invalid_data(self):
+        self._test_step_with_invalid_data(5, self.query_step_5, self.invalid_variables_step_5, 'studentProfileStep5',
+                                          ['nickname'])
 
-    def test_profile_step_6_with_invalid_state(self):
-        self._login()
-        self.user.profile_step = 6
-        self.user.save()
-        response = self.query(
-            '''
-            mutation StudentProfileMutation($step6: StudentProfileStep6Input) {
-              studentProfileStep6(step6: $step6) {
-                success,
-                errors
-              }
-            }
-            ''', variables={
-                'step6': {
-                    'state': 'anonymous2'
-                }
-            }
-        )
+    def test_profile_step_6_with_invalid_data(self):
+        self._test_step_with_invalid_data(6, self.query_step_6, self.invalid_variables_step_6, 'studentProfileStep6',
+                                          ['state'])
 
-        content = json.loads(response.content)
+    def test_profile_step_1(self):
+        self._test_and_get_step_response_content(1, self.query_step_1, self.variables_step_1, 'studentProfileStep1')
+        # reload user
+        user = get_user_model().objects.get(pk=self.student.pk)
+        self.assertEqual('John2', user.first_name)
+        self.assertEqual('Doe2', user.last_name)
 
-        self.assertResponseNoErrors(response)
-        self.assertIn('state', content['data'].get('studentProfileStep6').get('errors'))
+        profile = user.student
+        self.assertEqual(profile.street, 'Doestreet 55')
+        self.assertEqual(profile.zip, '9000')
+        self.assertEqual(profile.city, 'St. Gallen')
+        date_of_birth = datetime.strptime('01.01.2000', "%d.%m.%Y").date()
+        self.assertEqual(profile.date_of_birth, date_of_birth)
+        self.assertEqual(profile.mobile, '+41999999999')
+        self.assertEqual(user.profile_step, 2)
 
-    def test_profile_step_6(self):
-        self._login()
-        self.user.profile_step = 6
-        self.user.save()
-        response = self.query(
-            '''
-            mutation StudentProfileMutation($step6: StudentProfileStep6Input) {
-              studentProfileStep6(step6: $step6) {
-                success,
-                errors
-              }
-            }
-            ''', variables={
-                'step6': {
-                    'state': 'anonymous'
-                }
-            }
-        )
-
-        content = json.loads(response.content)
-
-        self.assertResponseNoErrors(response)
-        self.assertTrue(content['data'].get('studentProfileStep6').get('success'))
-        self.assertIsNone(content['data'].get('studentProfileStep6').get('errors'))
+    def test_profile_step_5(self):
+        self._test_and_get_step_response_content(5, self.query_step_5, self.variables_step_5, 'studentProfileStep5')
 
         # reload user
-        user = get_user_model().objects.get(pk=self.user.pk)
+        user = get_user_model().objects.get(pk=self.student.pk)
+        profile = user.student
+        self.assertEqual(profile.nickname, 'jane_doe')
 
+    def test_profile_step_5_with_existing_nickname(self):
+        content = self._test_and_get_step_response_content(5, self.query_step_5, self.invalid_variables_step_5_nickname,
+                                                           'studentProfileStep5', False)
+        errors = content['data'].get('studentProfileStep5').get('errors')
+        self.assertIn('nickname', errors)
+        self.assertIsNotNone(content['data'].get('studentProfileStep5').get('nicknameSuggestions'))
+
+    def test_profile_step_6(self):
+        self._test_and_get_step_response_content(6, self.query_step_6, self.variables_step_6, 'studentProfileStep6')
+        # reload user
+        user = get_user_model().objects.get(pk=self.student.pk)
         self.assertEqual(user.state, 'anonymous')
         self.assertEqual(user.profile_step, 7)
