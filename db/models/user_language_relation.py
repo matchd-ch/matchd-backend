0c5aaac7a7527a702955e3655087da1fc2ce5a2b
NoelJoost <noel.joost@ksb-sg.ch>
2021-02-15 09:59:52 +0100

    MATCHD-75: adapt user language relation Imports

--- db/models/user_language_relation.py
+++ db/models/user_language_relation.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-from db.models import Student
-
 
 class UserLanguageRelation(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey('db.Student', on_delete=models.CASCADE)
     language = models.ForeignKey('db.Language', on_delete=models.CASCADE)
     language_level = models.ForeignKey('db.LanguageLevel', on_delete=models.CASCADE)
 
