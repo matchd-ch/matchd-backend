d3be6bb41540c499aa469a243df643b6e3058dd0
Stefan Suter <stefan.suter@liip.ch>
2021-02-15 11:55:43 +0100

    MATCHD-75: fix cyclic imports

--- db/models/distinction.py
+++ db/models/distinction.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-from db.models import Student
-
 
 class Distinction(models.Model):
     text = models.CharField(max_length=255)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey('db.Student', on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('text', 'student',)
