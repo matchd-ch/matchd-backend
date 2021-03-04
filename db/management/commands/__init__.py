6b701658ed8468303ca8f0f4e046e8ab5ff19f0c
NoelJoost <noel.joost@ksb-sg.ch>
2021-03-04 14:50:56 +0100

    MATCHD-120: add load branches

---
+++ db/management/commands/__init__.py
@@ -0,0 +1,1 @@
+from .load_branches import Command
--- db/management/commands/load_branches.py
+++ db/management/commands/load_branches.py
@@ -1,28 +1,43 @@
 import json
 
-from django.core.management.base import BaseCommand, CommandError
-from et_xmlfile.tests.common_imports import read_file
+from django.core.management.base import BaseCommand
+
+from db.models import Branch
 
 
 class Command(BaseCommand):
     help = 'loads Branches in DB'
 
-    def handle(self, *args, **options):
-        dataSet = self.read_json()
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--clear',
+            action='store_true',
+            help='Clear Database before filling',
+        )
 
-        for data in dataSet:
+    def handle(self, *args, **options):
+        if options['clear']:
+            self.clear_branches()
+            self.stdout.write(self.style.SUCCESS('Cleared Branches'))
+            return
+
+        data_set = self.read_json()
+
+        for data in data_set:
             Branch.objects.create(
                 id=data["id"],
                 name=data["name"]
-            )
-
-            self.stdout.write(self.style.SUCCESS('IT WORKS YEAHY')
+            ).save()
+        self.stdout.write(self.style.SUCCESS('Filled Database'))
 
     def read_file(self):
-        file = open("../data/branches.json", "r")
+        file = open("db/management/data/branches.json", "r")
         data = file.read()
         file.close()
         return data
 
     def read_json(self):
         return json.loads(self.read_file())
+
+    def clear_branches(self):
+        Branch.objects.all().delete()
