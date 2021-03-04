1238ef7ffd173f81ad34210b10cd307254b56c9f
NoelJoost <noel.joost@ksb-sg.ch>
2021-03-04 15:55:15 +0100

    MATCHD-120: remove clear argument

--- db/management/commands/load_branches.py
+++ db/management/commands/load_branches.py
@@ -8,19 +8,7 @@ from db.models import Branch
 class Command(BaseCommand):
     help = 'loads Branches in DB'
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--clear',
-            action='store_true',
-            help='Clear Database before filling',
-        )
-
     def handle(self, *args, **options):
-        if options['clear']:
-            self.clear_branches()
-            self.stdout.write(self.style.SUCCESS('Cleared Branches'))
-            return
-
         data_set = self.read_json()
 
         for data in data_set:
@@ -38,6 +26,3 @@ class Command(BaseCommand):
 
     def read_json(self):
         return json.loads(self.read_file())
-
-    def clear_branches(self):
-        Branch.objects.all().delete()
