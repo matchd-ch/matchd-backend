39f9e661abdded8e8b5ddb926f20a065863b75b0
NoelJoost <noel.joost@ksb-sg.ch>
2021-03-08 09:57:03 +0100

    MATCHD-120: adapt management command

--- db/management/commands/load_branches.py
+++ db/management/commands/load_branches.py
@@ -1,38 +1,62 @@
 import json
-
+import glob
 from django.core.management.base import BaseCommand
-
 from db.models import Branch
+from django.apps import apps
+
+
 
 
 class Command(BaseCommand):
     help = 'loads Branches in DB'
 
     def handle(self, *args, **options):
-        data_set = self.read_json()
-        for data in data_set:
-            try:
-                branch_to_update = Branch.objects.get(id=data.get('pk'))
+        files = self.get_all_json()
+        for file in files:
 
-            except Branch.DoesNotExist:
-                branch_to_update = None
-            if branch_to_update is not None:
-                branch_to_update.name = data.get('fields').get('name')
-                branch_to_update.save()
+            data_set = self.read_json(file)
 
-            else:
-                Branch.objects.create(
-                    id=data.get('pk'),
-                    name=data.get('fields').get('name')
-                ).save()
+            for data in data_set:
+                self.get_fields(data)
+                model = self.get_model(data)
+                try:
+                    branch_to_update = model.objects.get(id=data.get('pk'))
 
-        self.stdout.write(self.style.SUCCESS('Filled Database'))
+                except model.DoesNotExist:
+                    branch_to_update = None
+                if branch_to_update is not None:
+                    branch_to_update.name = data.get('fields').get('name')
+                    branch_to_update.save()
 
-    def read_file(self):
-        with open("db/management/data/branches.json") as file:
+                else:
+                    model.objects.create(
+                        id=data.get('pk'),
+                        name=data.get('fields').get('name')
+                    ).save()
+
+            self.stdout.write(self.style.SUCCESS('Filled Database'))
+
+    def read_file(self, path):
+        # db/management/data/branches.json
+        with open(path) as file:
             data = file.read()
             file.close()
         return data
 
-    def read_json(self):
-        return json.loads(self.read_file())
+    def read_json(self, path):
+        return json.loads(self.read_file(path))
+
+    def get_all_json(self):
+        base_path = 'db/management/data'
+        return glob.glob(base_path + "/*.json")
+
+    def get_model(self, data):
+        data_model = data.get('model').split(".")
+        return apps.get_model(app_label='db', model_name=data_model[1])
+
+    def get_fields(self, data):
+        fields = data.get('fields').keys()
+        print(fields)
+        return fields
+
+
