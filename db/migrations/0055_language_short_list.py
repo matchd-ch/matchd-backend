6ecef18db67087b8be512635e50e81fe43399dfa
Stefan Suter <stefan.suter@liip.ch>
2021-03-16 12:36:22 +0100

    MATCHD-156: adapt migration order

--- db/migrations/0055_language_short_list.py
+++ db/migrations/0055_language_short_list.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('db', '0051_jobpostinglanguagerelation'),
+        ('db', '0054_jobpostinglanguagerelation'),
     ]
 
     operations = [
