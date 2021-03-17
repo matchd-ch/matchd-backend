e519a48c47582de6a08785217c57e9c0b05bd09a
NoelJoost <noel.joost@ksb-sg.ch>
2021-03-17 16:55:42 +0100

    MATCHD-181: add category blank false and null false to category model

--- db/models/category.py
+++ db/models/category.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=False, null=False)
 
     class Meta:
         ordering = ('name',)
