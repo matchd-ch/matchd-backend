2113f33583e0e20635b4802d603edd1cae335abe
NoelJoost <noel.joost@ksb-sg.ch>
2021-03-18 18:12:18 +0100

    MATCHD-171: add Soft Skills to admin

---
+++ db/admin/soft_skill.py
@@ -0,0 +1,17 @@
+from django.utils.translation import gettext_lazy as _
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+
+from db.models import SoftSkill
+
+
+class SoftSkillAdmin(ModelAdmin):
+    model = SoftSkill
+    menu_label = _('Soft Skills')
+    menu_icon = 'folder-open-inverse'
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ('student', 'company',)
+    search_fields = ('student', 'company')
+
+
+modeladmin_register(SoftSkillAdmin)
