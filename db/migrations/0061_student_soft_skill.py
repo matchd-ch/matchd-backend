60bc22c826a2f7a9351f3c83e47893f6c893f1cc
NoelJoost <noel.joost@ksb-sg.ch>
2021-03-22 16:50:13 +0100

    MATCHD-182: add soft skills to Me endpoint, adapt migration

--- db/migrations/0061_student_soft_skill.py
+++ db/migrations/0061_student_soft_skill.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('db', '0056_softskill'),
+        ('db', '0060_faqcategory'),
     ]
 
     operations = [
