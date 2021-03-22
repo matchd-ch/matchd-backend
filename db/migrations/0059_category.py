125b2c2281d902f77eda734e8775de9e30328c55
NoelJoost <noel.joost@ksb-sg.ch>
2021-03-22 10:20:43 +0100

    MATCHD-181: adapt migration order

--- db/migrations/0059_category.py
+++ db/migrations/0059_category.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('db', '0055_language_short_list'),
+        ('db', '0058_jobposting_employee'),
     ]
 
     operations = [
