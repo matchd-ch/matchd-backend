8df8ee9a7ede86edbf7ee64318e376bf3fee395e
Stefan Suter <stefan.suter@liip.ch>
2021-03-22 17:36:01 +0100

    MATCHD-50: rename validator

--- db/validators/student_profile.py
+++ db/validators/student_profile.py
@@ -1,11 +1,11 @@
 from django.core.exceptions import ValidationError
 
 
-class StudentProfileFormStepValidator:
+class ProfileFormStepValidator:
 
     def __init__(self, min_required_step=1):
         self.min_required_step = min_required_step
 
-    def validate(self, user):
-        if user.student.profile_step < self.min_required_step:
+    def validate(self, company_or_student):
+        if company_or_student.profile_step < self.min_required_step:
             raise ValidationError(code='invalid_step', message='You must first complete the previous steps.')
