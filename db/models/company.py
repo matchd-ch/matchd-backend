6d79fb53a7479bc4e4b75b927f0edc2f8bc948c6
Stefan Suter <stefan.suter@liip.ch>
2021-03-23 11:05:06 +0100

    MATCHD-50: add fields to company model

--- db/models/company.py
+++ db/models/company.py
@@ -8,8 +8,10 @@ from db.models.profile_state import ProfileState
 
 
 class Company(models.Model):
-    uid = models.CharField(max_length=255, blank=False,
-                           validators=[RegexValidator(regex=settings.UID_REGEX)])
+    # fields for company / university
+    state = models.CharField(choices=ProfileState.choices, max_length=255, blank=False, default=ProfileState.INCOMPLETE)
+    profile_step = models.IntegerField(default=1)
+    slug = models.SlugField(unique=True)
     name = models.CharField(max_length=255, blank=False)
     zip = models.CharField(max_length=10, blank=False)
     city = models.CharField(max_length=255, blank=False)
@@ -18,13 +20,18 @@ class Company(models.Model):
     website = models.URLField(max_length=2048, blank=True)
     branch = models.ForeignKey('db.Branch', blank=True, null=True, on_delete=models.DO_NOTHING)
     description = models.TextField(max_length=1000, blank=True)
+
+    # fields for company only
+    uid = models.CharField(max_length=255, blank=False,
+                           validators=[RegexValidator(regex=settings.UID_REGEX)])
     services = models.TextField(blank=True)
     member_it_st_gallen = models.BooleanField(blank=True, default=False)
     benefits = models.ManyToManyField('db.Benefit', related_name='benefits')
     job_positions = models.ManyToManyField('db.JobPosition', related_name='job_positions')
-    slug = models.SlugField(unique=True)
-    state = models.CharField(choices=ProfileState.choices, max_length=255, blank=False, default=ProfileState.INCOMPLETE)
-    profile_step = models.IntegerField(default=1)
+
+    # fields for university only
+    top_level_organisation_description = models.TextField(max_length=1000, blank=True)
+    top_level_organisation_url = models.URLField(max_length=2048, blank=True)
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
