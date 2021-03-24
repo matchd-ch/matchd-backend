12e3103405fd36c589b449fa024761adc2c62655
NoelJoost <noel.joost@ksb-sg.ch>
2021-03-24 14:26:24 +0100

    MATCHD-183: add Company Profile Step 4

--- api/schema/company/schema.py
+++ api/schema/company/schema.py
@@ -11,12 +11,14 @@ from api.schema.benefit import BenefitInput
 from api.schema.branch.schema import BranchInput
 from api.schema.employee import Employee
 from api.schema.job_position import JobPositionInput
+from api.schema.soft_skill import SoftSkillInputType
 from api.schema.profile_state import ProfileState
 from api.schema.profile_type import ProfileType
 from db.exceptions import FormException
 from db.forms import process_company_form_step_2, process_company_form_step_3, process_university_form_step_1, \
     process_university_form_step_2, process_university_form_step_3
 from db.forms.company_step_1 import process_company_form_step_1
+from db.forms.company_step_4 import process_company_form_step_4
 from db.models import Company as CompanyModel, ProfileState as ProfileStateModel
 
 
@@ -108,10 +110,34 @@ class CompanyProfileStep3(Output, graphene.Mutation):
         return CompanyProfileStep3(success=True, errors=None)
 
 
+class CompanyProfileInputStep4(graphene.InputObjectType):
+    soft_skills = graphene.List(SoftSkillInputType, description=_('Soft Skills'))
+
+
+class CompanyProfileStep4(Output, graphene.Mutation):
+    class Arguments:
+        step4 = CompanyProfileInputStep4(description=_('Profile Input Step 4 is required.'), required=True)
+
+    class Meta:
+        description = _('Updates the Company Profile with Soft Skills')
+
+    @classmethod
+    @login_required
+    def mutate(cls, root, info, **data):
+        user = info.context.user
+        form_data = data.get('step4', None)
+        try:
+            process_company_form_step_4(user, form_data)
+        except FormException as exception:
+            return CompanyProfileStep4(success=False, errors=exception.errors)
+        return CompanyProfileStep4(success=True, errors=None)
+
+
 class CompanyProfileMutation(graphene.ObjectType):
     company_profile_step1 = CompanyProfileStep1.Field()
     company_profile_step2 = CompanyProfileStep2.Field()
     company_profile_step3 = CompanyProfileStep3.Field()
+    company_profile_step4 = CompanyProfileStep4.Field()
 
 
 class UniversityProfileInputStep1(graphene.InputObjectType):
