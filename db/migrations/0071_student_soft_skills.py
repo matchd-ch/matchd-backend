ffc1de2b908ec2ddbefec245769500bdc3cdd72e
NoelJoost <noel.joost@ksb-sg.ch>
2021-03-25 18:33:05 +0100

    MATCHD-182: fix Migration order

--- db/migrations/0071_student_soft_skills.py
+++ db/migrations/0071_student_soft_skills.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('db', '0061_auto_20210317_1219'),
+        ('db', '0070_auto_20210324_1218'),
     ]
 
     operations = [
