36ac81b3f785e5244c75d3f7a82295b9267c70fc
Stefan Suter <stefan.suter@liip.ch>
2021-03-29 14:42:09 +0200

    MATCHD-77: adapt attachments view

--- db/helper/attachment.py
+++ db/helper/attachment.py
@@ -1,24 +1,23 @@
-from db.models import ProfileState
+from db.models import ProfileState, ProfileType
 
 
 def has_access_to_attachments(user, owner):
-    # owner can be a user or a company
     # check if user has a public profile, or the user is the owner of the attachments
     owner_is_company = True
-    if hasattr(owner, 'username'):
+    if owner.type != ProfileType.COMPANY:
         owner_is_company = False
 
     has_access = False
 
     if owner_is_company:
         # company
-        company_users = owner.users.all()
+        company_users = owner.company.users.all()
         if user in company_users:
             has_access = True
         else:
             # check if the company has a completed profile
             # company attachment are accessible for anonymous and public profile
-            state = owner.state
+            state = owner.company.state
             if state != ProfileState.INCOMPLETE:
                 has_access = True
     else:
