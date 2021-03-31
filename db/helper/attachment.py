baa6be26a28af8acc70645499c5589d4cfc6d8a3
Stefan Suter <stefan.suter@liip.ch>
2021-03-31 09:17:22 +0200

    MATCHD-243: adapt profile type check

--- db/helper/attachment.py
+++ db/helper/attachment.py
@@ -34,6 +34,6 @@ def has_access_to_attachments(user, owner):
 
 
 def get_company_or_student(user):
-    if user.type == ProfileType.COMPANY:
+    if user.type in ProfileType.valid_company_types():
         return user.company
     return user.student
