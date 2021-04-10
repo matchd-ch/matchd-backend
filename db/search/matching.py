05de0c00fbe3886e7de6c17407a62a95d765287d
Stefan Suter <stefan.suter@liip.ch>
2021-04-11 00:42:50 +0200

    MATCHD-14: adapt resolver

--- db/search/matching.py
+++ db/search/matching.py
@@ -1,7 +1,7 @@
 from wagtail.search.backends import get_search_backend
 
-from db.models import Student, Company, DateMode
-from db.search.builders import StudentParamBuilder, CompanyParamBuilder
+from db.models import Student, Company, DateMode, JobPosting
+from db.search.builders import StudentParamBuilder, CompanyParamBuilder, JobPostingParamBuilder
 from db.search.resolvers import HitResolver
 
 
@@ -12,7 +12,7 @@ class Matching:
     def find_talents_by_job_posting(self, job_posting, first=100, skip=0, soft_boost=1, tech_boost=1):
         queryset = Student.get_indexed_objects().prefetch_related('user')
         index = self.search_backend.get_index_for_model(queryset.model).name
-        maximum_score = self.calculate_maximum_job_posting_score(job_posting, soft_boost, tech_boost)
+        maximum_score = self.calculate_talent_matching_max_score(job_posting, soft_boost, tech_boost)
 
         builder = StudentParamBuilder(queryset, index, first, skip)
         builder.set_branch(job_posting.branch_id, 10)
@@ -34,6 +34,7 @@ class Matching:
         resolver = HitResolver(queryset, hits, maximum_score)
         return resolver.resolve()
 
+    # currently disabled
     def find_companies(self, branch_id=None, cultural_fits=None, soft_skills=None, job_type_id=None, soft_boost=1,
                        tech_boost=1, first=100, skip=0):
         queryset = Company.get_indexed_objects()
@@ -51,22 +52,64 @@ class Matching:
         resolver = HitResolver(queryset, hits, 1000)
         return resolver.resolve()
 
-    def find_job_postings(self):
-        # queryset = JobPosting.get_indexed_objects()
-        pass
+    def find_job_postings(self, user, job_type=None, workload=None, first=100, skip=0, soft_boost=1, tech_boost=1):
+        queryset = JobPosting.get_indexed_objects()
+        index = self.search_backend.get_index_for_model(queryset.model).name
 
-    def calculate_maximum_job_posting_score(self, job_posting, soft_boost, tech_boost):
+        # if no job type was submitted, we use the job type of the student
+        if job_type is None:
+            job_type = user.student.job_type
+
+        maximum_score = self.calculate_job_posting_matching_max_score(user, job_type, soft_boost, tech_boost)
+        builder = JobPostingParamBuilder(queryset, index, first, skip)
+        if job_type is not None:
+            builder.set_job_type(job_type.id, 10)
+        builder.set_skills(user.student.skills.all(), tech_boost)
+        if workload is not None:
+            builder.set_workload(workload, 1)
+        # languages = user.student.languages.all()
+        # if languages is not None:
+        #     builder.set_languages(languages)
+        date_mode = job_type.mode
+        if date_mode == DateMode.DATE_RANGE:
+            builder.set_date_range(user.student.job_from_date, user.student.job_to_date)
+        else:
+            builder.set_date_from(user.student.job_from_date)
+        hits = self.search_backend.es.search(**builder.get_params())
+        resolver = HitResolver(queryset, hits, maximum_score)
+        return resolver.resolve()
+
+    def calculate_talent_matching_max_score(self, job_posting, soft_boost, tech_boost):
         # Calculate the maximum score possible
-        # branch id, job type id
+        # 10 for branch id, 10 for job type id
         maximum_score = 20
         maximum_score += (len(job_posting.company.cultural_fits.all()) * soft_boost)
         maximum_score += (len(job_posting.company.soft_skills.all()) * soft_boost)
         maximum_score += (len(job_posting.skills.all()) * tech_boost)
         # matching on language level is disabled for now, see db.search.builders.student (set_languages)
-        # we do not add the score for the language, to get better matching scores
         # maximum_score += (len(job_posting.languages.all()) * 2)
         if job_posting.job_type.mode == DateMode.DATE_RANGE:
             maximum_score += 6  # max score for date or date range match
         else:
             maximum_score += 3  # max score for date or date range match
+        return maximum_score
+
+    def calculate_job_posting_matching_max_score(self, user, job_type, soft_boost, tech_boost):
+        # Calculate the maximum score possible
+        # 10 for job type id
+        maximum_score = 10
+        maximum_score += 3  # workload
+        # TODO cultural fits and soft skills
+        # maximum_score += (len(job_posting.company.cultural_fits.all()) * soft_boost)
+        # maximum_score += (len(job_posting.company.soft_skills.all()) * soft_boost)
+
+        maximum_score += (len(user.student.skills.all()) * tech_boost)
+        # matching on language level is disabled for now, see db.search.builders.student (set_languages)
+        # maximum_score += (len(user.student.languages.all()))
+
+        if job_type is not None:
+            if user.student.job_type.mode == DateMode.DATE_RANGE:
+                maximum_score += 6  # max score for date or date range match
+            else:
+                maximum_score += 3  # max score for date or date range match
         return maximum_score
--- db/search/resolvers/hit.py
+++ db/search/resolvers/hit.py
@@ -10,6 +10,7 @@ class HitResolver:
         self.raw_score_multiplier = 1
         self.maximum_possible_score = maximum_possible_score
         self.calculate_raw_score_multiplier()
+        self.override_score = False
 
     def resolve(self):
         scores = {}
@@ -26,7 +27,7 @@ class HitResolver:
             if float(score) < min_score:
                 min_score = score
 
-        # this mostly happens when only 1 result is returned
+        # this mostly happens if all results have the same score
         if max_score == min_score:
             self.calculate_score_multiplier(self.maximum_possible_score, min_score)
         else:
@@ -38,7 +39,10 @@ class HitResolver:
         for obj in result:
             obj_id = str(obj.id)
             if obj_id in scores:
-                setattr(obj, 'score', self.shift_score(float(scores[obj_id]), min_score))
+                if not self.override_score:
+                    setattr(obj, 'score', self.shift_score(float(scores[obj_id]), min_score))
+                else:
+                    setattr(obj, 'score', 1)
                 setattr(obj, 'raw_score', self.shift_raw_score(round(float(scores[obj_id]), 2)))
 
         def sort_by_score(value):
@@ -46,6 +50,12 @@ class HitResolver:
         return sorted(list(result), key=sort_by_score, reverse=True)
 
     def calculate_score_multiplier(self, max_score, min_score):
+        # this only happens if all hits have the same score, which is equal to max score
+        # eg. only 100% matches
+        if max_score - min_score == 0:
+            self.score_multiplier = 1
+            self.override_score = True
+            return
         self.score_multiplier = 100 / (max_score - min_score) / 100
 
     def calculate_raw_score_multiplier(self):
