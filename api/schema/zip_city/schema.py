4460787ed9289adde91a74c650f366eeb17037d2
Stefan Suter <stefan.suter@liip.ch>
2021-04-10 21:58:09 +0200

    MATCHD-14: add endpoint with zips from job postings

--- api/schema/zip_city/schema.py
+++ api/schema/zip_city/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene import ObjectType
 
 from api.data import zip_city_datasource
+from db.models import JobPosting, JobPostingState
 
 
 class ZipCity(ObjectType):
@@ -12,6 +13,17 @@ class ZipCity(ObjectType):
 
 class ZipCityQuery(ObjectType):
     zip_city = graphene.NonNull(graphene.List(graphene.NonNull(ZipCity)))
+    zip_city_jobs = graphene.NonNull(graphene.List(graphene.NonNull(ZipCity)))
 
     def resolve_zip_city(self, info, **kwargs):
         return zip_city_datasource.data
+
+    def resolve_zip_city_jobs(self, info, **kwargs):
+        job_postings = JobPosting.objects.select_related('company').filter(state=JobPostingState.PUBLIC).\
+            only('company__zip').values_list('company__zip', flat=True)
+        zip_values = list(job_postings)
+        zip_data = zip_city_datasource.data
+        return [obj for obj in zip_data if str(obj.get('zip')) in zip_values]
+
+
+
