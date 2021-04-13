48631a717119bee206e46467da48426ff5876f92
NoelJoost <noel.joost@ksb-sg.ch>
2021-04-13 17:15:23 +0200

    MATCHD-34: adapt DeleteFAQ and UpdateFAQ

--- db/forms/faq.py
+++ db/forms/faq.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext as _
+from graphql_jwt.exceptions import PermissionDenied
 
 from db.exceptions import FormException
 from db.helper import validate_form_data
@@ -51,15 +52,13 @@ def process_add_faq(user, data):
     faq_to_save.save()
 
 
-class UpdateFAQForm(forms.Form):
-    faq_id = forms.ModelChoiceField(queryset=FAQ.objects.all(), required=True)
-    category = forms.ModelChoiceField(queryset=FAQCategory.objects.all(), required=True)
-    title = forms.CharField(max_length=250, required=True)
-    question = forms.CharField(max_length=1000, required=True)
-    answer = forms.CharField(max_length=1000, required=True)
+class UpdateFAQForm(forms.ModelForm):
+    class Meta:
+        model = FAQ
+        fields = ['category', 'title', 'question', 'answer']
 
     def __init__(self, data=None, **kwargs):
-        # due to a bug with ModelChoiceField and graphene_django
+        # due to a bug with ModelForm and graphene_django
         data['category'] = convert_object_to_id(data.get('category', None))
         super().__init__(data=data, **kwargs)
 
@@ -71,65 +70,34 @@ def process_update_faq(user, data):
     errors = {}
     company = user.company
 
+    faq_to_save = get_object_or_404(FAQ, pk=data.get('faq_id'))
+    # check if company matches
+    if not faq_to_save.company == company:
+        raise PermissionDenied('You do not have permission to perform this action')
+
     # validate profile data
-    form = UpdateFAQForm(data)
+    form = UpdateFAQForm(data, instance=faq_to_save)
     form.full_clean()
 
     if form.is_valid():
-        cleaned_data = form.cleaned_data
-        faq_to_save = get_object_or_404(FAQ, pk=cleaned_data.get('faq_id').id)
-
-        # Check if Company Id is the same
-
-        if not faq_to_save.company == company:
-            errors.update(generic_error_dict('FAQG', _('Company doesn\'t match'), 'company_not_same'))
-
-        # required parameters
-        faq_to_save.category = cleaned_data.get('category')
-        faq_to_save.title = cleaned_data.get('title')
-        faq_to_save.question = cleaned_data.get('question')
-        faq_to_save.answer = cleaned_data.get('answer')
-        faq_to_save.company = company
+        form.save()
 
     else:
         errors.update(form.errors.get_json_data())
 
     if errors:
         raise FormException(errors=errors)
-
-    # save FAQ
-    faq_to_save.save()
-
-
-class DeleteFAQForm(forms.Form):
-    faq_id = forms.ModelChoiceField(queryset=FAQ.objects.all(), required=True)
 
 
 def process_delete_faq(user, data):
     # validate user type and data
     validate_company_user_type(user, ProfileType.COMPANY)
     validate_form_data(data)
-    errors = {}
     company = user.company
 
-    # validate profile data
-    form = DeleteFAQForm(data)
-    form.full_clean()
-
-    if form.is_valid():
-        cleaned_data = form.cleaned_data
-        faq_to_remove = get_object_or_404(FAQ, pk=cleaned_data.get('faq_id').id)
-
-        # Check if Company Id is the same
-
-        if not faq_to_remove.company == company:
-            errors.update(generic_error_dict('FAQ', _('Company doesn\'t match'), 'company_not_same'))
-
+    faq_to_remove = get_object_or_404(FAQ, pk=data.get('faq_id'))
+    # check if company matches
+    if faq_to_remove.company == company:
+        faq_to_remove.delete()
     else:
-        errors.update(form.errors.get_json_data())
-
-    if errors:
-        raise FormException(errors=errors)
-
-    # save FAQ
-    faq_to_remove.delete()
+        raise PermissionDenied('You do not have permission to perform this action')
