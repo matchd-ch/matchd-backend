112ea0d7b2b7200fc00d02bd40d5c01af8f6aebf
NoelJoost <noel.joost@ksb-sg.ch>
2021-04-13 14:48:27 +0200

    MATCHD-34: add updateFAQ

--- api/schema/faq/schema.py
+++ api/schema/faq/schema.py
@@ -8,7 +8,7 @@ from graphql_jwt.decorators import login_required
 
 from api.schema.faq_category.schema import FAQCategory, FAQCategoryInput
 from db.exceptions import FormException
-from db.forms.faq import process_faq
+from db.forms.faq import process_add_faq, process_update_faq
 from db.models import FAQ as FAQmodel, Company, FAQCategory as FAQCategoryModel
 
 
@@ -27,7 +27,7 @@ class FAQQuery(ObjectType):
         return faq_categories
 
 
-class FAQInput(graphene.InputObjectType):
+class AddFAQInput(graphene.InputObjectType):
     category = graphene.Field(FAQCategoryInput, description=_('category'), required=True)
     title = graphene.String(description=_('title'), required=True)
     question = graphene.String(description=_('question'), required=True)
@@ -36,10 +36,10 @@ class FAQInput(graphene.InputObjectType):
 
 class AddFAQ(Output, graphene.Mutation):
     class Arguments:
-        add_faq = FAQInput(description=_('FAQ input is required'), required=True)
+        add_faq = AddFAQInput(description=_('FAQ input is required'), required=True)
 
     class Meta:
-        description = _('Updates FAQ from your company')
+        description = _('add FAQ to your company')
 
     @classmethod
     @login_required
@@ -47,11 +47,39 @@ class AddFAQ(Output, graphene.Mutation):
         user = info.context.user
         form_data = data.get('add_faq', None)
         try:
-            process_faq(user, form_data)
+            process_add_faq(user, form_data)
         except FormException as exception:
             return AddFAQ(success=False, errors=exception.errors)
         return AddFAQ(success=True, errors=None)
 
 
+class UpdateFAQInput(graphene.InputObjectType):
+    faq_id = graphene.ID(description=_('Faq ID'), required=True)
+    category = graphene.Field(FAQCategoryInput, description=_('category'), required=True)
+    title = graphene.String(description=_('title'), required=True)
+    question = graphene.String(description=_('question'), required=True)
+    answer = graphene.String(description=_('answer'), required=True)
+
+
+class UpdateFAQ(Output, graphene.Mutation):
+    class Arguments:
+        update_faq = UpdateFAQInput(description=_('FAQ input is required'), required=True)
+
+    class Meta:
+        description = _('updates FAQ from your company')
+
+    @classmethod
+    @login_required
+    def mutate(cls, root, info, **data):
+        user = info.context.user
+        form_data = data.get('update_faq', None)
+        try:
+            process_update_faq(user, form_data)
+        except FormException as exception:
+            return UpdateFAQ(success=False, errors=exception.errors)
+        return UpdateFAQ(success=True, errors=None)
+
+
 class FAQMutation(graphene.ObjectType):
     add_faq = AddFAQ.Field()
+    update_faq = UpdateFAQ.Field()
--- db/forms/faq.py
+++ db/forms/faq.py
@@ -1,12 +1,14 @@
 from django import forms
+from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext as _
 
 from db.exceptions import FormException
 from db.helper import validate_form_data
-from db.helper.forms import validate_company_user_type, convert_object_to_id
+from db.helper.forms import validate_company_user_type, convert_object_to_id, generic_error_dict
 from db.models import ProfileType, FAQCategory, FAQ
 
 
-class FAQForm(forms.Form):
+class AddFAQForm(forms.Form):
     category = forms.ModelChoiceField(queryset=FAQCategory.objects.all(), required=True)
     title = forms.CharField(max_length=250, required=True)
     question = forms.CharField(max_length=1000, required=True)
@@ -18,16 +20,15 @@ class FAQForm(forms.Form):
         super().__init__(data=data, **kwargs)
 
 
-def process_faq(user, data):
-    print(FAQCategory.objects.all())
-    # validate user type, step and data
+def process_add_faq(user, data):
+    # validate user type and data
     validate_company_user_type(user, ProfileType.COMPANY)
     validate_form_data(data)
     errors = {}
     company = user.company
 
     # validate profile data
-    form = FAQForm(data)
+    form = AddFAQForm(data)
     form.full_clean()
     faq_to_save = FAQ()
     if form.is_valid():
@@ -48,5 +49,55 @@ def process_faq(user, data):
 
 
     # save FAQ
+    faq_to_save.save()
 
+
+class UpdateFAQForm(forms.Form):
+    faq_id = forms.ModelChoiceField(queryset=FAQ.objects.all(), required=True)
+    category = forms.ModelChoiceField(queryset=FAQCategory.objects.all(), required=True)
+    title = forms.CharField(max_length=250, required=True)
+    question = forms.CharField(max_length=1000, required=True)
+    answer = forms.CharField(max_length=1000, required=True)
+
+    def __init__(self, data=None, **kwargs):
+        # due to a bug with ModelChoiceField and graphene_django
+        data['category'] = convert_object_to_id(data.get('category', None))
+        super().__init__(data=data, **kwargs)
+
+
+def process_update_faq(user, data):
+    # validate user type and data
+    validate_company_user_type(user, ProfileType.COMPANY)
+    validate_form_data(data)
+    errors = {}
+    company = user.company
+
+    # validate profile data
+    form = UpdateFAQForm(data)
+    form.full_clean()
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        faq_to_save = get_object_or_404(FAQ, pk=cleaned_data.get('faq_id').id)
+
+        # Check if Company Id is the same
+
+        if not faq_to_save.company == company:
+            errors.update(generic_error_dict('FAQG', _('Company doesn\'t match'), 'company_not_same'))
+
+        # required parameters
+        faq_to_save.category = cleaned_data.get('category')
+        faq_to_save.title = cleaned_data.get('title')
+        faq_to_save.question = cleaned_data.get('question')
+        faq_to_save.answer = cleaned_data.get('answer')
+        faq_to_save.company = company
+
+    else:
+        errors.update(form.errors.get_json_data())
+
+
+    if errors:
+        raise FormException(errors=errors)
+
+    # save FAQ
     faq_to_save.save()
