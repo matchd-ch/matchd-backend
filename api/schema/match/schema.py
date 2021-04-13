8cfa918c953671e0dac2e746bb4b208d54c2e644
Stefan Suter <stefan.suter@liip.ch>
2021-04-13 12:37:09 +0200

    MATCHD-14: adapt job posting / student matching

--- api/schema/match/schema.py
+++ api/schema/match/schema.py
@@ -1,8 +1,4 @@
 import graphene
-from django.core.exceptions import PermissionDenied
-from django.db.models import Prefetch
-from django.http import Http404
-from django.shortcuts import get_object_or_404
 from graphql_jwt.decorators import login_required
 from graphene import ObjectType, InputObjectType
 
@@ -10,8 +6,7 @@ from api.schema.branch import BranchInput
 from api.schema.zip_city import ZipCityInput
 from api.schema.job_posting import JobPostingInput
 from api.schema.job_type import JobTypeInput
-from db.models import JobPosting as JobPostingModel, JobPostingLanguageRelation, JobType as JobTypeModel,  \
-    ProfileType as ProfileTypeModel, JobPostingState, MatchType as MatchTypeModel, Branch as BranchModel
+from db.models import MatchType as MatchTypeModel
 from db.search.matching import JobPostingMatching, StudentMatching
 
 MatchType = graphene.Enum.from_enum(MatchTypeModel)
@@ -62,68 +57,11 @@ class MatchQuery(ObjectType):
 
         job_posting_matching = kwargs.get('job_posting_matching', None)
         if job_posting_matching is not None:
-            return MatchQuery.job_posting_matching(user, job_posting_matching, first, skip, tech_boost, soft_boost)
+            matching = JobPostingMatching(user, job_posting_matching, first, skip, tech_boost, soft_boost)
+            return matching.find_matches()
 
         student_matching = kwargs.get('student_matching', None)
         if student_matching is not None:
-            return MatchQuery.student_matching(user, student_matching, first, skip, tech_boost, soft_boost)
+            matching = StudentMatching(user, student_matching, first, skip, tech_boost, soft_boost)
+            return matching.find_matches()
         return []
-
-    # pylint: disable=R0913
-    # pylint: disable=W0707
-    @classmethod
-    def student_matching(cls, user, data, first, skip, tech_boost, soft_boost):
-        if user.type not in ProfileTypeModel.valid_company_types():
-            raise PermissionDenied('You do not have the permission to perform this action')
-
-        job_posting_id = data.get('job_posting').get('id')
-        try:
-            job_posting = JobPostingModel.objects.prefetch_related(
-                Prefetch(
-                    'languages',
-                    queryset=JobPostingLanguageRelation.objects.filter(job_posting_id=job_posting_id).select_related(
-                        'language', 'language_level')
-                )
-            ).select_related('company').get(pk=job_posting_id)
-        except JobPostingModel.DoesNotExist:
-            raise Http404('Job posting does not exist')
-
-        if job_posting.state != JobPostingState.PUBLIC:
-            raise PermissionDenied('You do not have the permission to perform this action')
-
-        job_posting_company = job_posting.company
-        if user.company != job_posting_company:
-            raise PermissionDenied('You do not have the permission to perform this action')
-
-        matching = StudentMatching()
-        return matching.find_matches(job_posting, first, skip, soft_boost, tech_boost)
-
-    # pylint: disable=R0913
-    @classmethod
-    def job_posting_matching(cls, user, data, first, skip, tech_boost, soft_boost):
-        if user.type not in ProfileTypeModel.valid_student_types():
-            raise PermissionDenied('You do not have the permission to perform this action')
-
-        job_type_id = data.get('job_type', None)
-        if job_type_id is not None:
-            job_type_id = job_type_id.get('id')
-
-        job_type = None
-        if job_type_id is not None:
-            job_type = get_object_or_404(JobTypeModel, pk=job_type_id)
-
-        branch_id = data.get('branch', None)
-        if branch_id is not None:
-            branch_id = branch_id.get('id')
-
-        branch = None
-        if branch_id is not None:
-            branch = get_object_or_404(BranchModel, pk=branch_id)
-
-        workload = data.get('workload', None)
-        zip_value = data.get('zip', None)
-        if zip_value is not None:
-            zip_value = zip_value.get('zip', None)
-
-        matching = JobPostingMatching()
-        return matching.find_matches(user, job_type, branch, workload, zip_value, first, skip, soft_boost, tech_boost)
--- db/search/matching/job_posting.py
+++ db/search/matching/job_posting.py
@@ -1,7 +1,9 @@
 from django.conf import settings
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
 from wagtail.search.backends import get_search_backend
 
-from db.models import DateMode, JobPosting
+from db.models import DateMode, JobPosting, ProfileType, JobType, Branch
 from db.search.builders import JobPostingParamBuilder
 from db.search.calculators import JobPostingScoreCalculator
 from db.search.mapper import MatchMapper
@@ -12,34 +14,69 @@ from db.search.resolvers import HitResolver
 class JobPostingMatching:
     search_backend = get_search_backend()
 
-    def find_matches(self, user, job_type=None, branch=None, workload=None, zip_value=None, first=100, skip=0,
-                     soft_boost=1, tech_boost=1):
+    def __init__(self, user, data, first, skip, tech_boost, soft_boost):
+        self.user = user
+        self.data = data
+        self.job_type = None
+        self.branch = None
+        self.workload = 100
+        self.zip_value = None
+        self.first = first
+        self.skip = skip
+        self.tech_boost = tech_boost
+        self.soft_boost = soft_boost
+
+    def _validate_input(self):
+        if self.user.type not in ProfileType.valid_student_types():
+            raise PermissionDenied('You do not have the permission to perform this action')
+        job_type_id = self.data.get('job_type', None)
+        if job_type_id is not None:
+            job_type_id = job_type_id.get('id')
+        if job_type_id is not None:
+            self.job_type = get_object_or_404(JobType, pk=job_type_id)
+        if self.job_type is None:
+            self.job_type = self.user.student.job_type
+        branch_id = self.data.get('branch', None)
+        if branch_id is not None:
+            branch_id = branch_id.get('id')
+        if branch_id is not None:
+            self.branch = get_object_or_404(Branch, pk=branch_id)
+        if self.branch is None:
+            self.branch = self.user.student.branch
+
+        workload = self.data.get('workload', None)
+        if workload is not None:
+            self.workload = workload
+
+        zip_value = self.data.get('zip', None)
+        if zip_value is not None:
+            self.zip_value = zip_value.get('zip', None)
+
+    def find_matches(self):
+        self._validate_input()
         queryset = JobPosting.get_indexed_objects()
         index = self.search_backend.get_index_for_model(queryset.model).name
-        if job_type is None:
-            job_type = user.student.job_type
-        if branch is None:
-            branch = user.student.branch
-        if workload is None:
-            workload = 100
-        builder = JobPostingParamBuilder(queryset, index, first, skip)
-        builder.set_branch(branch.id, settings.MATCHING_VALUE_BRANCH)
-        builder.set_job_type(job_type.id, settings.MATCHING_VALUE_JOB_TYPE)
-        builder.set_cultural_fits(user.student.cultural_fits.all(), soft_boost * settings.MATCHING_VALUE_CULTURAL_FITS)
-        builder.set_soft_skills(user.student.soft_skills.all(), soft_boost * settings.MATCHING_VALUE_SOFT_SKILLS)
-        builder.set_skills(user.student.skills.all(), tech_boost * settings.MATCHING_VALUE_SKILLS)
-        builder.set_workload(workload, settings.MATCHING_VALUE_WORKLOAD)
-        if zip_value is not None:
-            builder.set_zip(zip_value)
-        date_mode = job_type.mode
+
+        builder = JobPostingParamBuilder(queryset, index, self.first, self.skip)
+        builder.set_branch(self.branch.id, settings.MATCHING_VALUE_BRANCH)
+        builder.set_job_type(self.job_type.id, settings.MATCHING_VALUE_JOB_TYPE)
+        builder.set_cultural_fits(self.user.student.cultural_fits.all(),
+                                  self.soft_boost * settings.MATCHING_VALUE_CULTURAL_FITS)
+        builder.set_soft_skills(self.user.student.soft_skills.all(),
+                                self.soft_boost * settings.MATCHING_VALUE_SOFT_SKILLS)
+        builder.set_skills(self.user.student.skills.all(), self.tech_boost * settings.MATCHING_VALUE_SKILLS)
+        builder.set_workload(self.workload, settings.MATCHING_VALUE_WORKLOAD)
+        if self.zip_value is not None:
+            builder.set_zip(self.zip_value)
+        date_mode = self.job_type.mode
         if date_mode == DateMode.DATE_RANGE:
-            builder.set_date_range(user.student.job_from_date, user.student.job_to_date,
+            builder.set_date_range(self.user.student.job_from_date, self.user.student.job_to_date,
                                    settings.MATCHING_VALUE_DATE_OR_DATE_RANGE)
         else:
-            builder.set_date_from(user.student.job_from_date, settings.MATCHING_VALUE_DATE_OR_DATE_RANGE)
+            builder.set_date_from(self.user.student.job_from_date, settings.MATCHING_VALUE_DATE_OR_DATE_RANGE)
         hits = self.search_backend.es.search(**builder.get_params())
         resolver = HitResolver(queryset, hits)
         hits = resolver.resolve()
-        calculator = JobPostingScoreCalculator(user, hits, soft_boost, tech_boost)
+        calculator = JobPostingScoreCalculator(self.user, hits, self.soft_boost, self.tech_boost)
         hits = calculator.annotate()
         return MatchMapper.map_job_postings(hits)
