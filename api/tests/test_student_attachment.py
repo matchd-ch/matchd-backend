1ec105c87b22b022681b2fe5f3ec3fa7e38773be
Stefan Suter <stefan.suter@liip.ch>
2021-04-22 17:15:19 +0200

    MATCHD-256: add tests

--- api/tests/test_student_attachment.py
+++ api/tests/test_student_attachment.py
@@ -1,6 +1,7 @@
 import pytest
 
-from db.models import AttachmentKey, ProfileState
+from db.models import AttachmentKey, ProfileState, Match
+
 
 # pylint: disable=R0913
 
@@ -90,3 +91,56 @@ def test_public_attachments(login, user_student, upload, file_image_jpg, attachm
     assert errors is None
     assert data is not None
     assert len(data.get('studentAvatar')) == 1
+
+
+@pytest.mark.django_db
+def test_protected_attachments(login, user_student, upload, file_document_pdf, attachments_for_user, logout,
+                                user_employee, query_attachments_for_slug, job_posting_object):
+    user_student.student.state = ProfileState.PUBLIC
+    user_student.student.save()
+    login(user_student)
+    data, errors = upload(user_student, AttachmentKey.STUDENT_DOCUMENTS, file_document_pdf)
+    assert data is not None
+    assert errors is None
+    assert data.get('upload') is not None
+    assert data.get('upload').get('success')
+
+    attachments = attachments_for_user(user_student, AttachmentKey.STUDENT_DOCUMENTS)
+    assert len(attachments) == 1
+
+    data, errors = query_attachments_for_slug(user_student, user_student.student.slug)
+    assert errors is None
+    assert data is not None
+    assert len(data.get('studentDocuments')) == 1
+
+    logout()
+    login(user_employee)
+
+    data, errors = query_attachments_for_slug(user_employee, user_student.student.slug)
+    assert errors is None
+    assert data is not None
+    assert len(data.get('studentDocuments')) == 0
+
+    job_posting_object.employee = user_employee.employee
+    job_posting_object.company = user_employee.company
+    job_posting_object.save()
+
+    match_obj = Match.objects.create(student=user_student.student, job_posting=job_posting_object,
+                                     company_confirmed=True, initiator=user_employee.type)
+
+    # employee should still have no access to student documents
+    data, errors = query_attachments_for_slug(user_employee, user_student.student.slug)
+    assert errors is None
+    assert data is not None
+    assert len(data.get('studentDocuments')) == 0
+
+    match_obj.delete()
+
+    Match.objects.create(student=user_student.student, job_posting=job_posting_object, student_confirmed=True,
+                         initiator=user_student.type)
+
+    # employee should have access to student documents
+    data, errors = query_attachments_for_slug(user_employee, user_student.student.slug)
+    assert errors is None
+    assert data is not None
+    assert len(data.get('studentDocuments')) == 1
