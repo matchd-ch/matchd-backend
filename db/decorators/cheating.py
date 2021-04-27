29d3f6771fb03dbe509bc9a497afed86e9c59d35
Stefan Suter <stefan.suter@liip.ch>
2021-04-27 18:16:44 +0200

    MATCHD-277: show protected fields for user of the same company

--- db/decorators/cheating.py
+++ db/decorators/cheating.py
@@ -1,8 +1,13 @@
 from api.helper import is_me_query
+from db.models import ProfileType
 
 
 def cheating_protection(func):
     def wrapper(self, info):
+        user = info.context.user
+        if user.type in ProfileType.valid_company_types():
+            if user.company == self.company:
+                return func(self, info)
         if is_me_query(info):
             return func(self, info)
         return None
