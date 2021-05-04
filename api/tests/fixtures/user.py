8c5dad1da690e03d97c52ad28a54cbee7bb9c1fe
Stefan Suter <stefan.suter@liip.ch>
2021-05-04 18:09:17 +0200

    MATCHD-257: fix pylint issue

--- api/tests/fixtures/user.py
+++ api/tests/fixtures/user.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 @pytest.fixture
 def get_user():
     def closure(username, password, verified, user_type, company=None):
+        # pylint: disable=W0612
         user, created = get_user_model().objects.get_or_create(
             username=username,
             email=username,
