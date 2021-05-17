7bd811746a23f97e04d63863e7bfb1bf73bab490
Stefan Suter <stefan.suter@liip.ch>
2021-05-17 15:05:23 +0200

    MATCHD-318: use shortlist languages for job posting seeder

--- db/seed/job_posting.py
+++ db/seed/job_posting.py
@@ -59,7 +59,7 @@ class JobPosting(BaseSeed):
                 job_posting.save()
                 job_posting.job_requirements.set(self.rand.requirements())
                 job_posting.skills.set(self.rand.skills())
-                languages = self.rand.languages()
+                languages = self.rand.languages_shortlist()
                 for language in languages:
                     JobPostingLanguageRelation.objects.create(
                         job_posting=job_posting, language_id=language.get('language'),
