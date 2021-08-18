5d14d534bca62eb38a302f3c06a1ea5b9f8067b4
Stefan Suter <stefan.suter@liip.ch>
2021-08-18 16:23:38 +0200

    MATCHD-401: delete companies in admin incl. all attachments

---
+++ db/admin/company.py
@@ -0,0 +1,19 @@
+from django.utils.translation import gettext_lazy as _
+from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+
+from db.models import Company
+from .permissions import ValidationPermissionHelper
+
+
+class CompanyAdmin(ModelAdmin):
+    model = Company
+    menu_label = _('Companies')
+    menu_icon = 'folder-open-inverse'
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ('name',)
+    search_fields = ('name',)
+    permission_helper_class = ValidationPermissionHelper
+
+
+modeladmin_register(CompanyAdmin)
