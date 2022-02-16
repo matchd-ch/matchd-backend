b0a1b760813d587afcc0add0896cfacf869e3012
Jonatan Männchen <jonatan@maennchen.ch>
2022-02-16 14:00:25 +0000

    Remove CSRF from API (#132)

--- api/views/csrf.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
-
-
-@csrf_exempt
-@ensure_csrf_cookie
-def csrf_view(request):
-    return HttpResponse(status=204)    # pragma: no cover
