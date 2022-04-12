7f0b98da096d9c8566e737671906700e09b95a84
Antonio <antonio.toncetti@gmail.com>
2022-04-12 02:55:48 +0200

    Fix broken link to static files caused by relative static file url. Solved with 'fullstatic' template tag returning absolute url. (#191)

--- db/templatetags/__init__.py
+++ db/templatetags/__init__.py
@@ -1,1 +1,2 @@
 from .db_tags import is_company, is_student
+from .static_tags import do_static
---
+++ db/templatetags/static_tags.py
@@ -0,0 +1,16 @@
+from django import template
+from django.templatetags import static
+
+register = template.Library()
+
+
+class FullStaticNode(static.StaticNode):
+
+    def url(self, context):
+        request = context['request']
+        return request.build_absolute_uri(super().url(context))
+
+
+@register.tag('fullstatic')
+def do_static(parser, token):
+    return FullStaticNode.handle_token(parser, token)
